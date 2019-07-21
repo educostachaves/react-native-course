6bb672c196fd4c97eff624c6d389031ad75b5266
Eduardo Chaves <eduardo.chaves@ibm.com>
2019-07-21 19:29:22 -0300

    upload files from sqipe project

---
+++ swipe/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+import Ball from './src/Ball';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Ball />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default App;
